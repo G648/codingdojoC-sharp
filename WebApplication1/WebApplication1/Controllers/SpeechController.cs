b42f4c6172de30fe47f236adb458364f5c123ceb
Wender <wendermaiac@gmail.com>
2024-04-17 10:36:37 -0300

    fix

--- WebApplication1/WebApplication1/Controllers/SpeechController.cs
+++ WebApplication1/WebApplication1/Controllers/SpeechController.cs
@@ -59,7 +59,7 @@ namespace WebApplication1.Controllers
 
             
 
-            return file;
+            return Ok(filename);
         }
 
 
--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d1aef8ac5022c89d6ac72e003b66a142249829b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a09891559017cd9b89a71deb51cd445caa59cf5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
